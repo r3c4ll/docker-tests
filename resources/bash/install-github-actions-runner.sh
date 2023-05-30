4ad8c7f9cf1fd500e4231dddbadd2f6e9c615d12
Ali Moreno <alimoreno@gmail.com>
2023-05-30 18:37:58 -0400

    Update install-github-actions-runner.sh

--- resources/bash/install-github-actions-runner.sh
+++ resources/bash/install-github-actions-runner.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Variables
+YOUR_NON-ROOT_SYSTEM_USER=
+YOUR_GITHUB_USERNAME=
+YOUR_GITHUB_REPOSITORY=
+YOUR_RUNNER_RESISTRATION_TOKEN=
+
 # Update system
 sudo apt update
 sudo apt upgrade -y
@@ -10,10 +16,10 @@ curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o 
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt update
 sudo apt install -y docker-ce docker-ce-cli containerd.io
-sudo usermod -aG docker YOUR_NON-ROOT_SYSTEM_USER
+sudo usermod -aG docker $YOUR_NON-ROOT_SYSTEM_USER
 
 # Verify Docker installation
-docker run hello-world
+sudo docker run hello-world
 
 # Install Docker Compose
 sudo apt install -y curl
@@ -24,10 +30,10 @@ sudo chmod +x /usr/local/bin/docker-compose
 docker-compose --version
 
 # Install, setup and run GitHub Action Runner as a system service
-mkdir actions-runner && cd actions-runner
+mkdir -p actions-runner && cd actions-runner && sudo rm -r *
 curl -O https://github.com/actions/runner/releases/download/v2.289.3/actions-runner-linux-x64-2.289.3.tar.gz
 tar xzf actions-runner-linux-x64-2.289.3.tar.gz
-./config.sh --url "https://github.com/YOUR_USERNAME/YOUR_GITHUB_REPOSITORY" --token "YOUR_RUNNER_RESISTRATION_TOKEN"
-sudo ./svc.sh install YOUR_NON-ROOT_SYSTEM_USER
+./config.sh --url "https://github.com/$YOUR_GITHUB_USERNAME/$YOUR_GITHUB_REPOSITORY" --token "$YOUR_RUNNER_RESISTRATION_TOKEN"
+sudo ./svc.sh install $YOUR_NON-ROOT_SYSTEM_USER
 ./svc.sh start
 ./svc.sh status
